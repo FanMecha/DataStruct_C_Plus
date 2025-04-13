54a6f20c66131486ec1386e6be6287c9589d868b
黄帆 <huangfan@huangfandeMacBook-Pro.local>
2025-04-13 23:11:21 +0800

    调整

--- src/algorithm/array.cpp
+++ src/algorithm/array.cpp
@@ -13,50 +13,47 @@
 //3.对于有序数组用二分搜索，其时间复杂度为O(logn)
 
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 
 class array{
 public:
-    array(int size = 10) : mCur_(0), mCap_(size){
-        mPtr_ = new int[mCap_]();
-    }
-    ~array(){
-        delete []mPtr_;
-        mPtr_ = nullptr;
+    array(int size = 10):mCur_(0),mCap_(size){
+        mPtr_ = new int[mCap_](); 
     }
 private:
-    //内部数组扩容接口
     void expand(int size){
         int *p = new int[size];
-        memcpy(p, mPtr_, sizeof(int)*mCur_);//按照字符拷贝
+        memcpy(p, mPtr_, sizeof(int)*mCur_);
+        //这里虽然删除了指针指向的内存，即指针不指向任何内存，但是指针本身还是存在的
         delete []mPtr_;
         mPtr_ = p;
         mCap_ = size;
     }
 public:
-    //末尾位置增加元素
+    //在数组尾部插入元素
     void push_back(int val){
-        //如果数组容量已满，先扩容
         if(mCur_ == mCap_){
             expand(2*mCap_);
         }
         mPtr_[mCur_] = val;
         mCur_++;
     }
-    //末尾位置删除元素
+    //删除数组尾部元素
     void pop_back(){
         if(mCur_ == 0){
             return;
         }
         //这里并没有真的删除最后一个元素,而是直接将元素的个数减1,这样打印时就认为数组里没有最后一个数据了
-        mCur_--; 
+        mCur_--;
     }
-    //按位置增加元素
+    //在数组指定位置插入元素
     void insert(int pos, int val){
         if(pos < 0 || pos > mCur_){
             return;
         }
-        if (mCur_ == mCap_){
-            expand(2*mCur_);
+        if(mCur_ == mCap_){
+            expand(2*mCap_);
         }
         for(int i = mCur_; i > pos; i--){
             mPtr_[i] = mPtr_[i-1];
@@ -64,56 +61,107 @@ public:
         mPtr_[pos] = val;
         mCur_++;
     }
-    //按位置删除元素
+    //删除数组指定位置元素
     void erase(int pos){
         if(pos < 0 || pos > mCur_){
             return;
         }
-        for(int i = pos; mCur_ >= i; i++){
+        for(int i = pos; i <= mCur_; i++){
             mPtr_[i] = mPtr_[i+1];
         }
         mCur_--;
     }
     //查找元素
     int find(int val){
-        for(int i = 0; i < mCur_; i++){
+        for (int i = 0; i< mCur_; i++){
             if(mPtr_[i] == val){
                 return i;
             }
         }
         return -1;
     }
-    void showArray(){
-        for(int i =0 ;i < mCur_; i++){
-            std::cout << mPtr_[i] << " ";
+    //===============双指针解决数组问题案例==============
+    //数组逆序
+    void reverse(){
+        int *p = mPtr_;
+        int *q = p + mCur_ - 1;
+        while(p < q){
+            int tmp = *p;
+            *p = *q;
+            *q = tmp;
+            p++;
+            q--;
         }
-        std::cout << std::endl;
+    }
+    //数组奇数放左边,偶数放右边
+    void adjustArray(){
+        int *p = mPtr_;
+        int *q = p + mCur_ - 1;
+        while(p < q){
+            //找偶数
+            while(p < q){
+                if(*p % 2 == 0){
+                    break;
+                }
+                p++;
+            }
+            //找奇数
+            while(p < q){
+                if(*q % 2 == 1){
+                    break;
+                }
+                q--;
+            }
+            if(p < q){
+                int tmp = *p;
+                *p = *q;
+                *q = tmp;
+                p++;
+                q--;
+            }
+        }
+    }
+    //打印数组
+    void showArray(){
+        for(int i = 0; i< mCur_; i++){
+            std::cout << mPtr_[i]<<" ";
+        }
+        std::cout <<std::endl;
     }
 private:
-    int *mPtr_;//指针指向堆内存上开辟的地址
-    int mCap_;//当前数组的容量
-    int mCur_;//当前数组的元素个数
+    int *mPtr_; //数组的地址
+    int mCur_; //数组元素的个数
+    int mCap_; //数组的容量
 };
-
 int main(){
-    // 末尾增加元素测试
-    array array1(10);
-    for(int i = 0; i < 10; i++){
-        array1.push_back(i);
+    //末尾增加元素测试
+    array arr;
+    for (int i = 0; i < 10; i++){
+        int val = rand() % 100;
+        arr.push_back(val);
     }
-    array1.showArray();
+    arr.showArray();
 
     //末尾删除元素测试
-    array1.pop_back();
-    array1.showArray();
+    arr.pop_back();
+    arr.showArray();
 
-    //指定位置增加元素测试
-    array1.insert(3,10000);
-    array1.showArray();
+    //指定位置插入元素测试
+    arr.insert(3,10000);
+    arr.showArray();
 
     //指定位置删除元素测试
-    int pos = array1.find(2);
-    array1.erase(pos);
-    array1.showArray();
+    int pos = arr.find(10000);
+    arr.erase(pos);
+    arr.showArray();
+
+    //数组逆序测试
+    arr.reverse();
+    arr.showArray();
+
+    //奇偶数测试
+    arr.adjustArray();
+    arr.showArray();
+
     return 0;
 }
